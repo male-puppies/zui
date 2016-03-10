edd490ccba73c9675836c49d75de7ae83f74a006
zxv5 <254790952@qq.com>
2016-03-10 14:46:52 +0800

    bug

--- uciweb/root/etc/nginx/share/access.lua
+++ uciweb/root/etc/nginx/share/access.lua
@@ -6,7 +6,7 @@ local call = uri:find("/call/")
 
 local function read(path, func)
 	func = func and func or io.open
-	local fp = func(path, "rb")
+	local fp = func(path, "r")
 	if not fp then 
 		return 
 	end 
