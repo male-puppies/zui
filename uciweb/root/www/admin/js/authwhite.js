b2aa0e5f3c1639cb916befc53eceb7dd03da8658
zxv5 <254790952@qq.com>
2016-05-12 16:11:50 +0800

    add authwhite

---
+++ uciweb/root/www/admin/js/authwhite.js
@@ -0,0 +1,53 @@
+$(function(){
+	createInitModal();
+	initEvents();
+	initData();
+});
+
+function createInitModal() {
+	$("#modal_tips").modal({
+		"backdrop": "static",
+		"show": false
+	});
+}
+
+function initData() {
+	cgicall("WhiteListGet", function(d) {
+		if (d.status == 0 && typeof d.data != "undefined") {
+			var arr = dtObjToArray(d.data);
+			var len = arr.length + 2;
+			if (len < 15) {
+				len = 15
+			}
+			$("#auth_white").attr("rows", len).val(arr.join("\n"));
+		} else {
+			console.log("WhiteListGet error...");
+		}
+	})
+}
+
+function initEvents() {
+	$('.submit').on('click', saveConf);
+	$('[data-toggle="tooltip"]').tooltip();
+}
+
+function saveConf() {
+	var val = $("#auth_white").val();
+	var arr = val.split("\n");
+	var sarr = [];
+	for (var i = 0; i < arr.length; i++) {
+		var s = arr[i].replace(/\s/g,'');
+		if ($.trim(s) != "") {
+			sarr[i] = s;
+		}
+	}
+
+	cgicall('WhiteListSet', sarr, function(d) {
+		if (d.status == 0) {
+			initData();
+			createModalTips("保存成功！");
+		} else {
+			createModalTips("保存失败！");
+		}
+	});
+}
