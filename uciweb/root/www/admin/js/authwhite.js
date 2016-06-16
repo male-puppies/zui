54e96c8e21e1d8a7f17724b4975e2f33e37c3cec
zxv5 <254790952@qq.com>
2016-06-16 15:32:59 +0800

    添加微信认证白名单，样式调整

--- uciweb/root/www/admin/js/authwhite.js
+++ uciweb/root/www/admin/js/authwhite.js
@@ -15,19 +15,25 @@ function initData() {
 	cgicall("WhiteListGet", function(d) {
 		if (d.status == 0 && typeof d.data != "undefined") {
 			var arr = dtObjToArray(d.data);
-			var len = arr.length + 2;
-			if (len < 15) {
-				len = 15
-			}
-			$("#auth_white").attr("rows", len).val(arr.join("\n"));
+			$("#auth_white").val(arr.join("\n"));
 		} else {
 			console.log("WhiteListGet error...");
+		}
+	});
+	
+	cgicall("WechatWhiteListGet", function(d) {
+		if (d.status == 0 && typeof d.data != "undefined") {
+			var arr = dtObjToArray(d.data);
+			$("#wechat_white").val(arr.join("\n"));
+		} else {
+			console.log("WechatWhiteListGet error...");
 		}
 	})
 }
 
 function initEvents() {
 	$('.submit').on('click', saveConf);
+	$('.wsubmit').on('click', wsaveConf);
 	$('[data-toggle="tooltip"]').tooltip();
 }
 
@@ -44,6 +50,28 @@ function saveConf() {
 	}
 
 	cgicall('WhiteListSet', sarr, function(d) {
+		if (d.status == 0) {
+			initData();
+			createModalTips("保存成功！");
+		} else {
+			createModalTips("保存失败！");
+		}
+	});
+}
+
+function wsaveConf() {
+	var val = $("#wechat_white").val();
+	var arr = val.split("\n");
+	var sarr = [];
+	for (var i = 0; i < arr.length; i++) {
+		if ($.trim(arr[i]).length == 0) continue;
+		var s = arr[i].replace(/\s/g,'');
+		if (typeof s != "undefined" && s && $.trim(s) != "") {
+			sarr.push(s);
+		}
+	}
+
+	cgicall('WechatWhiteListSet', sarr, function(d) {
 		if (d.status == 0) {
 			initData();
 			createModalTips("保存成功！");
