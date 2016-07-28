530a0e2e28acd3a1a69a483cb4f3e8a3e7bf9748
ubuntu <Ubuntu>
2016-07-28 16:31:02 +0800

    BUGxx:support mac whitelist

--- uciweb/root/www/admin/js/authwhite.js
+++ uciweb/root/www/admin/js/authwhite.js
@@ -29,11 +29,21 @@ function initData() {
 			console.log("WechatWhiteListGet error...");
 		}
 	})
+	
+	cgicall("MacWhiteListGet", function(d) {
+		if (d.status == 0 && typeof d.data != "undefined") {
+			var arr = dtObjToArray(d.data);
+			$("#mac_white").val(arr.join("\n"));
+		} else {
+			console.log("MacWhiteListGet error...");
+		}
+	})
 }
 
 function initEvents() {
 	$('.submit').on('click', saveConf);
 	$('.wsubmit').on('click', wsaveConf);
+	$('.msubmit').on('click', msaveConf);
 	$('[data-toggle="tooltip"]').tooltip();
 }
 
@@ -80,3 +90,32 @@ function wsaveConf() {
 		}
 	});
 }
+
+function msaveConf() {
+	var val = $("#mac_white").val();
+	var arr = val.split("\n");
+	var sarr = [];
+	for (var i = 0; i < arr.length; i++) {		
+		if ($.trim(arr[i]).length == 0) continue;
+		var s = arr[i].replace(/\s/g,'');
+		if (typeof s != "undefined" && s && $.trim(s) != "") {
+			var reg_name = /[A-Fa-f0-9]{2}:[A-Fa-f0-9]{2}:[A-Fa-f0-9]{2}:[A-Fa-f0-9]{2}:[A-Fa-f0-9]{2}:[A-Fa-f0-9]{2}/;
+			if (!reg_name.test(s) || (s.length > 17)) {
+				createModalTips("mac地址格式不正确！mac地址格式为00:24:21:19:BD:E4");
+				return;
+			}  else{
+			sarr.push(s);
+			}
+		}
+	}
+
+	cgicall('MacWhiteListSet', sarr, function(d) {
+		if (d.status == 0) {
+			initData();
+			createModalTips("保存成功！");
+		} else {
+			createModalTips("保存失败！");
+		}
+	});
+}
+
